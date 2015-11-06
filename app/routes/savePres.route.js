98ee78f5f581a602bad75defa65725c80cf4ae5c
Seni Gueye <seni.gueye@mastertp>
2015-11-06 15:05:00 +0100

    add save presentation source code in savePres.route.js

--- app/routes/savePres.route.js
+++ app/routes/savePres.route.js
@@ -2,18 +2,24 @@
 
 var express = require("express");
 var jsonfile = require("jsonfile");
+var fs = require("fs");
 var router = express.Router();
 var CONFIG = JSON.parse(process.env.CONFIG);
-var relativePresentationDirectory = __dirname + CONFIG.contentDirectory;
+var relativePresentationDirectory = __dirname + CONFIG.presentationDirectory;
 
 module.exports = router;
 
 router.route("/")
-  .get(function(request, response) {
-    console.info("load save presentation route");
-    console.log(request);
-    json.send("load save presentation route")
-    // jsonfile.readFile(relativePresentationDirectory + "/", function(err, obj) {
-      // console.dir(obj)
-    // });
+  .post(function(request, response) {
+ 	var file = '[pres.id].pres.json';
+	var obj = "{\"name\: \"JP\"}";
+ 	
+	 fs.writeFile(relativePresentationDirectory + "/" + file, obj, function (err) {
+	  if (err) throw err;
+	  console.log('It\'s saved!');
+	});
+	//	fs.writeFileSync(CONFIG.presentationDirectory+"/"+file, obj);
+	    // jsonfile.readFile(relativePresentationDirectory + "/", function(err, obj) {
+	      // console.dir(obj)
+	    // });
   });
